0e398b8cecb9b2ff0da320842e43191f726ac7e0
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-07-07 17:03:18 +0200

    #2 create token with /api/auth/token

--- microservices/users_service/users_service/api/routes.py
+++ microservices/users_service/users_service/api/routes.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from datetime import datetime, timedelta
 
 api = Blueprint('api', __name__)
+
+
+
 
 @api.route('/test', methods=['GET'])
 def handle_hello():
@@ -9,3 +16,11 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/token', methods=['POST'])
+def create_token_jwt():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    token = create_access_token(identity=email,expires_delta=timedelta(minutes=30))
+    return jsonify(access_token=token)
--- microservices/users_service/users_service/app.py
+++ microservices/users_service/users_service/app.py
@@ -1,11 +1,16 @@
 
 from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 from api.routes import api
 
 app.register_blueprint(api, url_prefix='/api/auth')
+app.config['JWT_SECRET_KEY'] = "misecreto"
+
+jwt = JWTManager(app)
 
 
 if __name__ == '__main__':
